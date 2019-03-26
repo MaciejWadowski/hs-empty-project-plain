c3b2a66055b0bd0b0dbde68d9866cbc4aca0f72e
wadziuxxx <macwadowski@tlen.pl>
2019-03-26 22:41:10 +0100

    insert function works

--- dist/build/autogen/Paths_hs_empty_project_plain.hs
+++ dist/build/autogen/Paths_hs_empty_project_plain.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE CPP #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -fno-warn-implicit-prelude #-}
 module Paths_hs_empty_project_plain (
     version,
-    getBinDir, getLibDir, getDataDir, getLibexecDir,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
     getDataFileName, getSysconfDir
   ) where
 
@@ -9,22 +12,34 @@ import Data.Version (Version(..))
 import System.Environment (getEnv)
 import Prelude
 
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
 catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
 catchIO = Exception.catch
 
 version :: Version
 version = Version [0,1,0,0] []
-bindir, libdir, datadir, libexecdir, sysconfdir :: FilePath
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
 
-bindir     = "/home/students/w/wadomaci/Haskell/lab2/hs-empty-project-plain/.cabal-sandbox/bin"
-libdir     = "/home/students/w/wadomaci/Haskell/lab2/hs-empty-project-plain/.cabal-sandbox/lib/x86_64-linux-ghc-7.10.3/hs-empty-project-plain-0.1.0.0-4zVqq24rL6o8315TResALA"
-datadir    = "/home/students/w/wadomaci/Haskell/lab2/hs-empty-project-plain/.cabal-sandbox/share/x86_64-linux-ghc-7.10.3/hs-empty-project-plain-0.1.0.0"
-libexecdir = "/home/students/w/wadomaci/Haskell/lab2/hs-empty-project-plain/.cabal-sandbox/libexec"
-sysconfdir = "/home/students/w/wadomaci/Haskell/lab2/hs-empty-project-plain/.cabal-sandbox/etc"
+bindir     = "/home/maciej/IdeaProjects/hs-empty-project-plain/.cabal-sandbox/bin"
+libdir     = "/home/maciej/IdeaProjects/hs-empty-project-plain/.cabal-sandbox/lib/x86_64-linux-ghc-8.0.2/hs-empty-project-plain-0.1.0.0-KteVdRdTFUvKsIlUbfXJQG"
+dynlibdir  = "/home/maciej/IdeaProjects/hs-empty-project-plain/.cabal-sandbox/lib/x86_64-linux-ghc-8.0.2"
+datadir    = "/home/maciej/IdeaProjects/hs-empty-project-plain/.cabal-sandbox/share/x86_64-linux-ghc-8.0.2/hs-empty-project-plain-0.1.0.0"
+libexecdir = "/home/maciej/IdeaProjects/hs-empty-project-plain/.cabal-sandbox/libexec"
+sysconfdir = "/home/maciej/IdeaProjects/hs-empty-project-plain/.cabal-sandbox/etc"
 
-getBinDir, getLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
 getBinDir = catchIO (getEnv "hs_empty_project_plain_bindir") (\_ -> return bindir)
 getLibDir = catchIO (getEnv "hs_empty_project_plain_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "hs_empty_project_plain_dynlibdir") (\_ -> return dynlibdir)
 getDataDir = catchIO (getEnv "hs_empty_project_plain_datadir") (\_ -> return datadir)
 getLibexecDir = catchIO (getEnv "hs_empty_project_plain_libexecdir") (\_ -> return libexecdir)
 getSysconfDir = catchIO (getEnv "hs_empty_project_plain_sysconfdir") (\_ -> return sysconfdir)
--- src/Tree.hs
+++ src/Tree.hs
@@ -1,1 +1,15 @@
 module Tree where
+
+data Tree a = EmptyTree
+              | Node a (Tree a) (Tree a)
+              deriving(Show, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+insert::(Ord a) => a -> (Tree a) -> (Tree a)
+insert x EmptyTree = singleton x
+insert x (Node a left right)
+      | x == a = Node x left right
+      | x > a =  Node a left (insert x right)
+      | x < a = Node a (insert x left) right
