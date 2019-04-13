441a44c758352df0335deb1ea599f22b60abf10e
wadziuxxx <macwadowski@tlen.pl>
2019-04-13 19:08:17 +0200

    started exercise 4

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,15 @@ module Lib
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+sum' :: (Num a) => [a] -> a
+sum' x = foldl (+) 0 x
+
+product' :: (Num a) => [a] -> a
+product' x = foldl (*) 1 x
+
+and' :: [Bool] -> Bool
+and' x = foldl (&&) True x
+
+or' :: [Bool] -> Bool
+or' x = foldl (||) False x
