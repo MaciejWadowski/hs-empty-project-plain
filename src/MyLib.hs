ec1eb85f63e91a303c9f5267ea6db6c8bc15279c
wadziuxxx <macwadowski@tlen.pl>
2019-05-02 14:44:33 +0200

    fixed reading from file

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -11,42 +11,33 @@ processFile fileContent = toResult . selectMostFrequent 10 . countWords . toWord
 
 processWords :: WordList -> Char -> WordList
 processWords list ' ' = "" : list
+processWords list '\n' = "" : list
 processWords (x:xs) char = (x ++ [char]):xs
 processWords [] char = [[char]]
 
 toWordList :: String -> [String]
 toWordList longSentence = foldl processWords [] longSentence
 
+countWordsFoldl :: Map.Map String Int -> String -> Map.Map String Int
+countWordsFoldl map word
+        | Map.member word map == True = Map.insert word (fromJust (Map.lookup word map) + 1) map
+        | otherwise = Map.insert word 1 map
+
 countWords :: [String] -> Map.Map String Int
-countWords list = countWords' list Map.empty
+countWords list = foldl countWordsFoldl Map.empty list
 
 fromJust :: Maybe a -> a
 fromJust (Just a) = a
 
-countWords' :: [String] -> Map.Map String Int -> Map.Map String Int
-countWords' [] map = map
-countWords' (x:xs) map
-       | Map.member x map == True = countWords' xs (Map.insert x (((fromJust (Map.lookup x map))+1)) map)
-       | otherwise = countWords' xs (Map.insert x 1 map)
+selectMostFrequentFoldl :: Heap.MinHeap (String, Int) -> (String, Int)  -> Heap.MinHeap (String, Int)
+selectMostFrequentFoldl heap (word, count)
+    | count > intVal = Heap.insert (word,count) (Heap.drop 1 heap)
+    | otherwise = heap
+    where (str, intVal) = fromJust (Heap.viewHead heap)
 
 selectMostFrequent :: Int -> Map.Map String Int -> Heap.MinHeap (String, Int)
-selectMostFrequent n map = selectMostFrequent' n (Map.toList map) Heap.empty
-
-selectMostFrequent' :: Int -> [(String, Int)]-> Heap.MinHeap (String, Int) -> Heap.MinHeap (String, Int)
-selectMostFrequent' n list heap
-    | n == 0 = heap
-    | n > 0 = selectMostFrequent' (n-1) (removeItem list maxVal) (Heap.insert maxVal heap)
-    where maxVal = foldl maximums (head list) list
-
-removeItem :: [(String, Int)] -> (String, Int)-> [(String, Int)]
-removeItem ((x1,x2):xs) (y1,y2)
-    | x1 == y1 = xs
-    | otherwise = (x1,x2): removeItem xs (y1,y2)
-
-maximums :: (String, Int) -> (String, Int) -> (String, Int)
-maximums (x1,x2) (y1,y2)
-    | x2 > y2 = (x1,x2)
-    | otherwise = (y1,y2)
+selectMostFrequent n map = foldl selectMostFrequentFoldl (Heap.fromList (take n list)) list
+        where list = Map.toList map
 
 toResult :: Heap.MinHeap (String, Int) -> Result
 toResult heap = Heap.toList heap
